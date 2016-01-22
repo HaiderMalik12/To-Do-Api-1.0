db18f254a3df1833609b55f3f435e4ad361c54cc
Haider Malik <haidermalik5044@gmail.com>
2016-01-22 00:01:22 -0800

    Add POST:/todos

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 var express=require('express');
 var app=express();
+var bodyParser=require('body-parser');
 var PORT=process.env.PORT || 3000;
-var todos=[{
-	id:1,
-	description:"To-Do getting all todos",
-	completed:false
-},
-{
-id:2,
-description:'To-Do get todo by Id',
-completed:false	
-}];
+var todos=[];
+var todoNextId=1;
+//apply middleware
+
+app.use(bodyParser.json());
 
 //start routing
 
@@ -50,6 +46,19 @@ else{
 }
 });
 
+// POST /todos
+
+app.post('/todos',function(req,res){
+    var body=req.body;
+   
+    body.id=todoNextId++;
+
+    todos.push(body);
+
+    res.json(body);
+
+
+});
 //listening Express on PORT
 
 app.listen(PORT,function(){
