7e44e825a1052edb82e8f8f34d5f6c7942c6005b
Haider Malik <haidermalik5044@gmail.com>
2016-01-21 21:40:58 -0800

    Add GET/todos

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express=require('express');
 var app=express();
 var PORT=process.env.PORT || 3000;
-
+var todos=[{
+	id:1,
+	description:"To-Do getting all todos",
+	completed:false
+},
+{
+id:2,
+description:'To-Do get todo by Id',
+completed:false	
+}];
 
 //start routing
 
 app.get('/',function(req,res){
 
 res.send('To Do Api version no 1');
+});
+
+//GET :/todos
+
+app.get('/todos',function(req,res){
+res.json(todos);
 });
 
 
